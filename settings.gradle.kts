25e210629b69883349c9add3fc6d6b9f6311b696
ibrahim-zebary <ibrahim.zebary2018@gmail.com>
2022-10-05 18:44:25 +0300

    I-1 changed gradle files to kotlin dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         mavenCentral()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,4 +14,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "IMDB"
-include ':app'
+include(":app")
